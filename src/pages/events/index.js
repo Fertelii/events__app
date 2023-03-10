6a5047986fc6ce9fd2fc8a3c881129e998bd6493
Fertelii <94907419+Fertelii@users.noreply.github.com>
2023-03-10 19:18:22 +0200

    adding generated pages

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,30 +1,22 @@
-const EventsPage = () => {
+import Image from 'next/image'
+import Link from 'next/link'
+
+const EventsPage = ({ data }) => {
 
     return (
         <div>
             <h1>Events Page</h1>
             <div>
-                <a href=''>
-                    <img />
-                    <h2>Events in London</h2>
-                    <p>
-                        Laboris tempor eiusmod ad dolore ad elit et adipisicing consectetur nulla consequat. Et consequat minim anim excepteur amet aute eiusmod mollit pariatur anim sint. Magna velit non tempor nisi reprehenderit ipsum nulla. Aliquip commodo aliqua laboris tempor labore. Culpa sint eiusmod excepteur tempor proident pariatur exercitation magna irure. Amet aute sint amet magna sit non culpa enim sunt laborum culpa anim nostrud id. Do deserunt culpa cupidatat officia aute nostrud irure laboris anim quis cupidatat.
-                    </p>
-                </a>
-                <a href=''>
-                    <img />
-                    <h2>Events in San Francisco</h2>
-                    <p>
-                        Laboris tempor eiusmod ad dolore ad elit et adipisicing consectetur nulla consequat. Et consequat minim anim excepteur amet aute eiusmod mollit pariatur anim sint. Magna velit non tempor nisi reprehenderit ipsum nulla. Aliquip commodo aliqua laboris tempor labore. Culpa sint eiusmod excepteur tempor proident pariatur exercitation magna irure. Amet aute sint amet magna sit non culpa enim sunt laborum culpa anim nostrud id. Do deserunt culpa cupidatat officia aute nostrud irure laboris anim quis cupidatat.
-                    </p>
-                </a>
-                <a href=''>
-                    <img />
-                    <h2>Events in Barselona</h2>
-                    <p>
-                        Laboris tempor eiusmod ad dolore ad elit et adipisicing consectetur nulla consequat. Et consequat minim anim excepteur amet aute eiusmod mollit pariatur anim sint. Magna velit non tempor nisi reprehenderit ipsum nulla. Aliquip commodo aliqua laboris tempor labore. Culpa sint eiusmod excepteur tempor proident pariatur exercitation magna irure. Amet aute sint amet magna sit non culpa enim sunt laborum culpa anim nostrud id. Do deserunt culpa cupidatat officia aute nostrud irure laboris anim quis cupidatat.
-                    </p>
-                </a>
+                {data.map(ev => (
+                    <Link key={ev.id} href={`/events/${ev.id}`} passHref>
+                        <Image
+                            width={300}
+                            height={300}
+                            src={ev.image}
+                            alt={ev.title} />
+                        <h2>{ev.title}</h2>
+                    </Link>
+                ))}
             </div>
         </div>
     )
@@ -32,3 +24,12 @@ const EventsPage = () => {
 
 export default EventsPage
 
+export async function getStaticProps() {
+    const { events_categories } = await import('/data/data.json')
+
+    return {
+        props: {
+            data: events_categories,
+        },
+    }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,19 +18,19 @@ export default function Home({ data }) {
 
       <header>
         <nav>
-          <img />
-          <a href='/'>Home</a>
-          <a href='/events'>Events</a>
-          <a href='/about-us'>About us</a>
+          <Link href='/' passHref>Home</Link>
+          <Link href='/events' passHref>Events</Link>
+          <Link href='/about-us' passHref>About us</Link>
         </nav>
       </header>
       <main className={styles.main}>
-        {data.map(ev =>
-          <a key={ev.id} href={`/events/${ev.id}`}>
-            <Image width={200} height={400} src={ev.image} alt={ev.title} />
+        {data.map(ev => (
+          <Link key={ev.id} href={`/events/${ev.id}`} passHref>
+
+            <Image width={300} height={300} src={ev.image} alt={ev.title} />
             <h2>{ev.title}</h2>
             <p>{ev.description}</p>
-          </a>)
+          </Link>))
         }
 
       </main>
@@ -42,8 +43,8 @@ export default function Home({ data }) {
 
 
 export async function getServerSideProps() {
-  const { events_categories } = await import('../../data/data.json')
-  console.log(events_categories)
+  const { events_categories } = await import('/data/data.json')
+
   return {
     props: {
       data: events_categories,
